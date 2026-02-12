f8828d8bb812dcd878790e588a3e91fbbe0ee3f9
Zidiny Erre <zidiny.erre.fr@gmail.com>
2026-02-12 23:53:08 +0100

    essaie .env

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 
+import os
 from typing import Union
-from fastapi import FastAPI
+from dotenv import load_dotenv, dotenv_values
+# from fastapi import FastAPI
 
-app = FastAPI()
+load_dotenv()
 
-@app.get("")
+print(os.getenv("MY_KEY"))
+# app = FastAPI()
+
+# @app.get("")
 
 
-def read_root():
-    return{"Hello": "World"}
+# def read_root():
+#     return{"Hello": "World"}
 
-@app.get("/items/{item_id}")
+# @app.get("/items/{item_id}")
 
-def read_item(item_id: int, q: Union[str, None] = None):
-    return { "item_id" : item_id, "q" : q}
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return { "item_id" : item_id, "q" : q}
